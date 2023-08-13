9e624297814e0d15c7ceb2511b91007ba8ce5523
Stedoss <29103029+Stedoss@users.noreply.github.com>
2023-08-13 01:25:45 +0100

    Don't double auth on tests; they will break

--- Yanoac.IntegrationTests/OsuClientV2Tests.cs
+++ Yanoac.IntegrationTests/OsuClientV2Tests.cs
@@ -6,21 +6,17 @@ namespace Yanoac.IntegrationTests;
 public class OsuClientV2Tests
 {
     [Test]
-    public async Task Authorise_AuthenticatesWithApi()
+    public void Authorise_AuthenticatesWithApi()
     {
         var client = ClientHelpers.AuthenticatedTestClient;
-
-        await client.Authorise();
 
         Assert.That(client.IsAuthenticated, Is.True);
     }
 
     [Test]
-    public async Task Authorise_ACG_AuthenticatedWithApi()
+    public void Authorise_ACG_AuthenticatedWithApi()
     {
         var client = ClientHelpers.AuthenticationCodeTestClient;
-        
-        await client.Authorise("http://localhost:4567/");
         
         Assert.That(client.IsAuthenticated, Is.True);
     }
