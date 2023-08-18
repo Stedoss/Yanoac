4643307fd9a7eb4ebe25c2ad2a4b9885e0f84009
Stedoss <29103029+Stedoss@users.noreply.github.com>
2023-08-18 22:17:08 +0100

    Add conversion tests of all modes for `UserScore`

--- Yanoac.IntegrationTests/Partials/ScoresFragmentTests.cs
+++ Yanoac.IntegrationTests/Partials/ScoresFragmentTests.cs
@@ -20,6 +20,14 @@ public class ScoresFragmentTests : AuthenticatedAuthCodeClientTest
         score.AsTestable().Should().BeEquivalentTo(testScore.AsTestable());
     }
 
+    [TestCaseSource(nameof(test_score_list))]
+    public async Task GetScore_ParsesWithoutErrors((string, long) score)
+    {
+        var getScore = async () => await Client.GetScore(score.Item1, score.Item2);
+
+        await getScore.Should().NotThrowAsync();
+    }
+
     [Test]
     public async Task ScoreDownload()
     {
@@ -27,4 +35,23 @@ public class ScoresFragmentTests : AuthenticatedAuthCodeClientTest
 
         Assert.That(replay, Is.Not.Null);
     }
+
+    private static readonly (string, long)[] test_score_list =
+    {
+        ("osu", 2716015058),
+        ("osu", 2702383046),
+        ("osu", 4491294030),
+        ("osu", 4478711346),
+        ("osu", 4491286592),
+        ("osu", 3948818994),
+        ("osu", 4298439387),
+        ("osu", 2177560145),
+        ("mania", 222046829),
+        ("mania", 491815877),
+        ("mania", 544800408),
+        ("taiko", 179488881),
+        ("taiko", 180342921),
+        ("catch", 208579641),
+        ("catch", 210593305),
+    };
 }
