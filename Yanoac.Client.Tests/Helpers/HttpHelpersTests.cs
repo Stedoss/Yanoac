2e647f751ebce789f30521b3f2c4adf25ea1a060
Stedoss <29103029+Stedoss@users.noreply.github.com>
2023-08-20 00:08:28 +0100

    Add tests for `EnumerableToUrlParams`

---
+++ Yanoac.Client.Tests/Helpers/HttpHelpersTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Yanoac.Client.Helpers;
+
+namespace Yanoac.Client.Tests.Helpers;
+
+[TestFixture]
+public class HttpHelpersTests
+{
+    [TestCase("ids", new[] { "1", "2", "3" }, "ids[]=1&ids[]=2&ids[]=3")]
+    [TestCase("ids", new[] { "one", "two", "three" }, "ids[]=one&ids[]=two&ids[]=three")]
+    [TestCase("ids", new[] { "one" }, "ids[]=one")]
+    [TestCase("ids", new string[] { }, "")]
+    public void EnumerableToUrlParams(string paramName, string[] parameters, string expected)
+    {
+        string urlParams = HttpHelpers.EnumerableToUrlParams(paramName, parameters);
+
+        Assert.That(urlParams, Is.EqualTo(expected));
+    }
+}
